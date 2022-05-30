fc652ba5a74f9c9923b15b00687c3adda3cafa49
Khushi Jain <106108676+khushi-jain15@users.noreply.github.com>
2022-05-30 14:15:56 +0530

    Create append.py

---
+++ numpy/append.py
@@ -0,0 +1,7 @@
+import numpy as npappend
+x = [100, 200, 300]
+print("Original array:")
+print(x)
+x = npappend.append(x, [[400, 500, 610], [700, 810, 900]])
+print("After append the values are:")
+print(x)
