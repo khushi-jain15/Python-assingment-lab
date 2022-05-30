c09eb956f57966320751c17e366b7c2986181313
Khushi Jain <106108676+khushi-jain15@users.noreply.github.com>
2022-05-30 14:41:55 +0530

    Create volume_area_sphere.py

---
+++ Modules/volume_area_sphere.py
@@ -0,0 +1,6 @@
+PI = 3.14
+radius = float(input("Enter the Radius of a Sphere: "))
+sa =  4 * PI * radius * radius
+Volume = (4 / 3) * PI * radius * radius * radius
+print("\nThe Surface area of a Sphere = %.2f" %sa)
+print("\nThe Volume of a Sphere = %.2f" %Volume)
